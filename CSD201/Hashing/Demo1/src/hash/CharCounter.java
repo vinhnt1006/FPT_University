bc9742be47ae927045f105ed77f8a3c1fabc5e86
NGUYEN TRAN VINH <vinhnt10062002@gmail.com>
2022-06-29 08:38:54 +0700

    first commit

--- CSD201/Hashing/Demo1/src/hash/CharCounter.java
+++ CSD201/Hashing/Demo1/src/hash/CharCounter.java
@@ -5,37 +5,35 @@ import java.util.HashMap;
 
 public class CharCounter extends HashMap<Integer, Integer> {
 
-    int numOfChars = 0;
-
-    public CharCounter() {
-        super();
-    }
+    int numOfCounts = 0;
 
     public CharCounter(String filename) {
         super();
         try {
             FileReader fr = new FileReader(filename);
             int code;
-            while ((code= fr.read())!=-1) {
-                if (!this.containsKey(code)) this.put(code, 1);
-                else this.replace(code, this.get(code)+1);
-                numOfChars++;
+            while ((code = fr.read()) != -1) {
+                if (!this.containsKey(code)) {
+                    this.put(code, 1);
+                } else {
+                    this.replace(code, this.get(code) + 1);
+                }
+                numOfCounts++;
             }
-        fr.close();
+            fr.close();
         } catch (Exception e) {
             System.out.println(e);
         }
     }
 
     public void print() {
-        for (Integer cObj:this.keySet()) {
-            System.out.println((char)cObj.intValue() + ", "
-                    + this.get(cObj) + ", " + 1.0*this.get(cObj)/numOfChars);
+        for (Integer cObj : this.keySet()) {
+            System.out.println((char) cObj.intValue() + ", " + this.get(cObj) + ", " + 1.0 * this.get(cObj) / numOfCounts);
         }
     }
 
     public static void main(String[] args) {
-        CharCounter counter= new CharCounter("laychong.txt");
+        CharCounter counter = new CharCounter("laychong.txt");
         counter.print();
     }
 }
---
+++ CSD201/Sorting/Sort/src/sort/IntSorterTest.java
@@ -0,0 +1,92 @@
+package sort;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class IntSorterTest {
+
+    public static final int SELECTION = 1, INSERTION = 2, BUBBLE = 3;
+    public static final int QUICK = 4, MERGE = 5, HEAP = 6, RADIX = 7;
+
+    public static void genArray(int a[], int n) {
+        Random ran = new Random(System.currentTimeMillis());
+        for (int i = 0; i < n; i++) {
+            a[i] = ran.nextInt(1000);
+        }
+    }
+
+    public static boolean checkAsc(int a[], int n) {
+        for (int i = 1; i < n; i++) {
+            if (a[i] < a[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static long measure(int a[], int n, int method) {
+        long t1 = System.currentTimeMillis();
+        long t2;
+        switch (method) {
+            case SELECTION:
+                IntSorter.selectionSort(a, n);
+                break;
+            case INSERTION:
+                IntSorter.insertionSort(a, n);
+                break;
+            case BUBBLE:
+                IntSorter.bubbleSort(a, n);
+                break;
+            case QUICK:
+                IntSorter.quickSort(a, n);
+                break;
+            case MERGE:
+                IntSorter.mergeSort(a, n);
+                break;
+            case HEAP:
+                IntSorter.heapSort(a, n);
+                break;
+            case RADIX:
+                IntSorter.radixSort(a, n);
+                break;
+        }
+        t2 = System.currentTimeMillis();
+        return t2 - t1;
+    }
+
+    public static void main(String[] args) {
+        int n = 100000;
+        int ar[] = new int[n];
+        genArray(ar, n);
+        System.out.println("Waiting for sorting " + n + " elements.\n");
+        long t;
+        int a[] = Arrays.copyOf(ar, n);
+        t = measure(a, n, SELECTION);
+        System.out.println("Selection sort, Time cost: " + t + " milisec.");
+        System.out.println("Ascending order? " + checkAsc(a, n));
+        a = Arrays.copyOf(ar, n);
+        t = measure(a, n, INSERTION);
+        System.out.println("Insertion sort, Time cost: " + t + " milisec.");
+        System.out.println("Ascending order? " + checkAsc(a, n));
+        a = Arrays.copyOf(ar, n);
+        t = measure(a, n, BUBBLE);
+        System.out.println("Bubble sort, Time cost: " + t + " milisec.");
+        System.out.println("Ascending order? " + checkAsc(a, n));
+        a = Arrays.copyOf(ar, n);
+        t = measure(a, n, QUICK);
+        System.out.println("Quick sort, Time cost: " + t + " milisec.");
+        System.out.println("Ascending order? " + checkAsc(a, n));
+        a = Arrays.copyOf(ar, n);
+        t = measure(a, n, MERGE);
+        System.out.println("Merge sort, Time cost: " + t + " milisec.");
+        System.out.println("Ascending order? " + checkAsc(a, n));
+        a = Arrays.copyOf(ar, n);
+        t = measure(a, n, HEAP);
+        System.out.println("Heap sort, Time cost: " + t + " milisec.");
+        System.out.println("Ascending order? " + checkAsc(a, n));
+        a = Arrays.copyOf(ar, n);
+        t = measure(a, n, RADIX);
+        System.out.println("Radix sort, Time cost: " + t + " milisec.");
+        System.out.println("Ascending order? " + checkAsc(a, n));
+    }
+}
